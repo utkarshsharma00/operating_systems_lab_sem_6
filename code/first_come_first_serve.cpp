86da94d5e67481c9a8ea95809385fcaafa9df033
Utkarsh Sharma <flamingice2801@gmail.com>
2020-02-08 10:47:27 +0530

    Create first_come_first_serve.cpp

---
+++ code/first_come_first_serve.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    int n, bt[20], wt[20], tat[20], avwt = 0, avtat = 0, i, j; 
+    // bt - burst time
+    // wt - waiting time
+    // tat - total average time
+    // avwt - average waiting time 
+    // avtat - average turn-around time
+    cout << "Enter total number of processes(maximum 20):";
+    cin >> n;
+    cout << "\nEnter Process Burst Time\n";
+
+    for (i = 0; i < n; i++)
+    {
+        cout << "P[" << i + 1 << "]:";
+        cin >> bt[i];
+    }
+
+    wt[0] = 0; //waiting time for first process is 0
+
+    //calculating waiting time
+    for (i = 1; i < n; i++)
+    {
+        wt[i] = 0;
+        for (j = 0; j < i; j++)
+            wt[i] += bt[j];
+    }
+
+    cout << "\nProcess\t\tBurst Time\tWaiting Time\tTurnaround Time";
+
+    //calculating turnaround time
+    for (i = 0; i < n; i++)
+    {
+        tat[i] = bt[i] + wt[i];
+        avwt += wt[i];
+        avtat += tat[i];
+        cout << "\nP[" << i + 1 << "]"
+             << "\t\t" << bt[i] << "\t\t" << wt[i] << "\t\t" << tat[i];
+    }
+
+    avwt /= i;
+    avtat /= i;
+    cout << "\n";
+    cout << "\nAverage Waiting Time:" << avwt;
+    cout << "\nAverage Turnaround Time:" << avtat;
+    // cout << "\nGantt Chart:"; //TODO
+    return 0;
+}
